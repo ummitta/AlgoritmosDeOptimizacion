668c1c4e64fc36d8fe8822bb4e0935759e6b5b1f
MaximoHumittas <maxmora401@gmail.com>
2025-05-15 21:54:36 -0400

    avance gradiente

--- gradiente.py
+++ gradiente.py
@@ -1,3 +1,4 @@
+
 #
 #algoritmo de gradiente descendente
 #
@@ -5,3 +6,67 @@
 import derivada as d
 import numpy as np 
 import sympy as sp
+
+#  x**2+y**2
+
+def Gradiente(vector,funcion,**kwars):
+    print(kwars)
+
+    x, y, z = sp.symbols('x y z')
+
+    funcion = sp.sympify(funcion)
+
+    xPuntos = kwars.get('x')
+    yPuntos = kwars.get('y')
+    xDelta = kwars.get('deltaX')
+    yDelta = kwars.get('deltaY')
+    # print(xPuntos)
+    # print(yPuntos)
+    # print(xDelta)
+    # print(yDelta)
+
+
+
+    gradienteSimbolica = []
+    gradienteAplicada = []
+    print("Gradiente simbolica")
+    for i in range(0,len(vector)):
+        derivadaEn = vector[i]
+        
+        funcionDerivada = sp.diff(funcion,derivadaEn)
+    
+        gradienteSimbolica.append(funcionDerivada)
+        print(f"{funcionDerivada}")
+
+    print("Gradiente evaluado en un punto")
+    for i in range(0,len(gradienteSimbolica)):
+
+        funcion = gradienteSimbolica[i]
+
+        for j in range(0,len(xPuntos)):
+         
+            valor = funcion.subs([(x,xPuntos[j]), (y,yPuntos[j])])
+            print(f"{funcion} ([{xPuntos[j]},{yPuntos[j]}]) = {valor}")
+            # print(valor)
+
+    
+    print("Aproximacion ")
+    for i in range(0,len(gradienteSimbolica)):
+
+        funcion = gradienteSimbolica[i]
+
+        for j in range(0,len(xPuntos)):
+
+            xPunto = xPuntos[j] + xDelta[j]
+            yPunto = yPuntos[j] + yDelta[j]
+            
+     
+            valor = funcion.subs([(x,xPunto), (y,yPunto)])
+            print(f"{funcion} ([{xPuntos[j]},{yPuntos[j]}]) = {valor}")
+          
+
+
+
+
+
+Gradiente(['x','y'],"x**2 + y**2",**{'x':[2],'y':[3],'deltaX':[0.01],'deltaY':[0.01]})

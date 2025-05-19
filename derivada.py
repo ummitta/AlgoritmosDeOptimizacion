915576868816e21cabd60c80e97241b41262a900
nikobarros <nbarros2019@alu.uct.cl>
2025-05-19 19:28:43 -0400

    final derivada

--- derivada.py
+++ derivada.py
@@ -1,53 +1,45 @@
-#
-# Derivada Numérica con estimación de Δx óptimo
-# Autor: Nicolás Barros D.
-#
-
 import numpy as np
 import sympy as sp
 import sys
 
 epsilon = sys.float_info.epsilon
-print(f"Valor de ε (error absoluto): {epsilon:.2e}\n")
 
-# x**3 + x + 1
+print(f"Valor de epsilon: {epsilon:.2e}\n")
+
+# ej: x**3 + x + 1
 f_input = input("Ingrese la función f(x): ")
 
 x_valor = float(input("Valor de x para evaluar la derivada: "))
-
-x_precision = 0
 
 x = sp.Symbol("x")
 f = sp.sympify(f_input)
 
 f_deriv = sp.diff(f, x)
+
 deriv_real = f_deriv.evalf(subs={x: x_valor})
 
 print(f"\nDerivada simbólica: f'({x_valor}) = {deriv_real}\n")
 
-# Probar distintos Δx
-print("Δx\t\tDerivada Numérica\tError Absoluto")
+print("Δx\t\tDerivada Numérica\tDerivada Aproximada\tError Absoluto")
+print("--------------------------------------------------------------------------------")
 
-mejor_dx = None
-
-
+derivada_real = f_deriv.evalf(subs={x: x_valor})
 dx = 1.0
 
 while dx > epsilon:
-    derivada_aprox = sp.limit((f.evalf(subs={x: x_valor + dx}) - f.evalf(subs={x: x_valor}))/dx, dx, 0)
+    derivada_aprox = sp.limit((f.evalf(subs={x: x_valor + dx}) - f.evalf(subs={x: x_valor}))/dx
 
-    derivada_real = f_deriv.evalf(subs={x: x_valor})
     error = abs(derivada_aprox - derivada_real)
-    print("derivada_aprox", derivada_aprox)
-    print("derivada_real", derivada_real)
-    print("error", error)
+
+    print(f"{dx:.2e}\t{derivada_real}\t{derivada_aprox}\t{error}")
 
     if error < epsilon:
         print(f"dx óptimo encontrado: {dx}")
         break
 
     dx /= 2
-
+print("Reporte de resultados:")
+print("--------------------------")
 print(f"dx óptimo: {dx}")
 print(f"Derivada numérica: {derivada_aprox}")
 print(f"Derivada analítica: {derivada_real}")

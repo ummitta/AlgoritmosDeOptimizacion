5f1249ce0bae3c7b149d16f312fedde41b801e20
nikobarros <nbarros2019@alu.uct.cl>
2025-06-09 21:01:32 -0400

    2.10 limpio

--- tallerSumativo2.10.py
+++ tallerSumativo2.10.py
@@ -4,9 +4,6 @@
 import sympy as sp
 import numpy as np
 
-#epsilon = np.finfo(float).eps
-
-
 def newton(funcion,puntoUsuario,t,epsilon):
 
     x = sp.symbols('x')
@@ -19,7 +16,6 @@ def newton(funcion,puntoUsuario,t,epsilon):
 
     funcionSimbolica = sp.sympify(funcion)
 
-    #print("funcionSimbolica: ", funcionSimbolica)
     punto = puntoUsuario
 
     verificacion = True
@@ -27,30 +23,20 @@ def newton(funcion,puntoUsuario,t,epsilon):
     while verificacion:
 
         gradientePunto = derivada.subs(x,punto)
-
         hessianaPunto = hessiana.subs(x,punto)
-
-        #print("derivada: ", derivada)
-
-        #print("hessiaana: ", hessiana)
 
         if hessianaPunto == 0:
             print("Nose se puede divir por 0")
 
         puntoNuevo = punto - t * (gradientePunto/hessianaPunto)
 
-        #print(f"punto: {punto} | gradiente: {gradientePunto} | hessiana: {hessianaPunto} | puntoNuevo: {puntoNuevo}  ")
         print(f" hessiana: {hessianaPunto} ")
-
         print(f"punto: {punto} | gradiente: {gradientePunto} | hessiana: {float(hessianaPunto):.4f}  | puntoNuevo: {puntoNuevo}  ")
 
         if sp.Abs(puntoNuevo - punto) <= epsilon:
-            #print("se rompeee")
-            #print(f"punto: {punto} | gradiente: {gradientePunto} | hessiana: {hessianaPunto} | puntoNuevo: {puntoNuevo}  ")
-
             verificacion = False
             continue
-        #puntoNuevo
+        
         punto = puntoNuevo
 
     return float(puntoNuevo)
